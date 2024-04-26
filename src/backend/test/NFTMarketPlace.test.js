b930712e90ae2bd98db682b5d2450ad7456ebcd9
chmuhammadasim <boybrave82@gmail.com>
2024-04-26 22:18:57 +0500

    Add URI variable and update NFTMarketPlace tests

--- src/backend/test/NFTMarketPlace.test.js
+++ src/backend/test/NFTMarketPlace.test.js
@@ -4,6 +4,7 @@ const { ethers } = require("hardhat");
 describe('NFTMarketPlace', function()  { 
     let deployer, addr1, addr2, nft, marketplace;
     let feePercent = 1;
+    let URI = "Simple URI"
     beforeEach( async function() {
         const NFT = await ethers.getContractFactory("NFT");
         const MarketPlace = await ethers.getContractFactory("MarketPlace");
@@ -15,6 +16,18 @@ describe('NFTMarketPlace', function()  {
         it('Should Track Name and Symbol of NFT Collection', async function() {
             expect(await nft.name()).equal("DApp NFT");
             expect(await nft.symbol()).equal("DAPP");
+        });
+        it('Should Track Fee Account and Fee Percent of MarketPlace', async function() {
+            expect(await marketplace.feeAccount()).equal(deployer.address);
+            expect(await marketplace.feePercent()).equal(feePercent);
+        });
+    })
+    describe("Minting NFTs", function() {
+        it("Should Track Each Minted NFT", async function() {
+            await nft.connect(addr1).mint(URI); 
+            expect(await nft.tokenCount()).equal(1);
+            expect(await nft.balanceOf(addr1.address)).equal(1);
+            expect(await nft.tokenURI(1)).equal(URI);
         })
     })
- })
+})
