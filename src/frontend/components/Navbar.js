d5bc6b427978cfd497e61ac37e49d7312286f8c0
chmuhammadasim <boybrave82@gmail.com>
2024-05-04 22:27:16 +0500

    chore: Update contract addresses and refactor code in frontend components

--- src/frontend/components/Navbar.js
+++ src/frontend/components/Navbar.js
@@ -1,6 +1,4 @@
-import {
-    Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Navbar, Nav, Button, Container } from 'react-bootstrap'
 import market from './market.png'
 
