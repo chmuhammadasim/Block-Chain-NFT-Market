e03d70620543504c574012f860a271addb07f056
chmuhammadasim <boybrave82@gmail.com>
2024-05-06 16:22:02 +0500

    chore: Update IPFS client URL in create.js and add sold items section to myListItem.js

--- src/frontend/components/create.js
+++ src/frontend/components/create.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import { ethers } from "ethers"
 import { Row, Form, Button } from 'react-bootstrap'
 import { create as ipfsHttpClient } from 'ipfs-http-client'
-const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/040253bf194b40d18b13182e320509bf/v0')
 
 const Create = ({ marketplace, nft }) => {
   const [image, setImage] = useState('')
   const [price, setPrice] = useState(null)
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
+
   const uploadToIPFS = async (event) => {
     event.preventDefault()
     const file = event.target.files[0]
